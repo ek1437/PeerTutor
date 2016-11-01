cf5080172e192c05a6875856d16359493cab004f
muhand <muhandjumah@gmail.com>
2016-11-01 17:44:12 -0400

    Handled navigation to logout.js

--- www.peertutor.com/controllers/logout.js
+++ www.peertutor.com/controllers/logout.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const Redirect = require('../middlewares/redirect');
 
 module.exports = {
   registerRouter() {
     const router = express.Router();
 
+    router.get('/', Redirect.ifNotLoggedIn('/'), this.index);
+      
     router.post('/', this.logout);
-
     return router;
+  },
+  index(req, res){
+    res.redirect('/');
   },
   logout(req, res) {
     req.logout();
