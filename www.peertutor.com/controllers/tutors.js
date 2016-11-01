4e459d34f937caf38ca259fffe722eb4043d741d
muhand <muhandjumah@gmail.com>
2016-11-01 17:06:28 -0400

    Removed extra white space in tutors.js

--- www.peertutor.com/controllers/tutors.js
+++ www.peertutor.com/controllers/tutors.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var models = require('../models');
 
-
 module.exports = {
   registerRouter() {
     const router = express.Router();
